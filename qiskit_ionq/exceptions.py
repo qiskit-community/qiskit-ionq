df9599418d369b0ecd037f538d871ae767694b4a
jon donovan <donovan@ionq.co>
2022-04-19 16:33:42 -0700

    fixup test, remove newlines from message

--- qiskit_ionq/exceptions.py
+++ qiskit_ionq/exceptions.py
@@ -129,8 +129,9 @@ class IonQGateError(IonQError, JobError):
     def __init__(self, gate_name):
         self.gate_name = gate_name
         super().__init__(
-            f"gate '{gate_name}' not supported on IonQ backends. "
-            "Please use the qiskit.transpile method or manually rewrite to remove the gate"
+            (f"gate '{gate_name}' not supported on IonQ backends. "
+             + "Please use the qiskit.transpile method or manually rewrite to remove the gate"
+            )
         )
 
     def __repr__(self):
