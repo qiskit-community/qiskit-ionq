88f2d61003832dc65f4c8b735a2160e547a63a39
Spencer Churchill <churchill@ionq.co>
2023-12-18 11:15:10 -0800

    update revision version

--- qiskit_ionq/version.py
+++ qiskit_ionq/version.py
@@ -34,7 +34,7 @@ from typing import List
 pkg_parent = pathlib.Path(__file__).parent.parent.absolute()
 
 # major, minor, micro
-VERSION_INFO = ".".join(map(str, (0, 4, 4)))
+VERSION_INFO = ".".join(map(str, (0, 4, 5)))
 
 
 def _minimal_ext_cmd(cmd: List[str]) -> bytes:
